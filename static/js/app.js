e735bef9a89b8ad8c22a5e0706138bb3c3ad494e
roudyb3 <roudyb3@gmail.com>
2021-11-22 19:09:00 -0500

    first try

---
+++ static/js/app.js
@@ -0,0 +1,46 @@
+// from data.js
+var tableData = data;
+
+//reference the table body
+var tbody = d3.select("tbody");
+
+//use a for each to loop through the data.js file and add in the text into the table body
+data.forEach(function(ufoSightings) {
+    console.log(ufoSightings)
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSightings).forEach(function([key,value]) {
+        console.log(key, value);
+        // append the cell to a row for each value in the object
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+//form filter
+//select button and form
+var form = d3.select("#filters");
+var button = d3.select("#filter-btn");
+
+//create event handlers
+form.on("change", runEnter);
+button.on("click", runEnter);
+
+//complete the event handler function for the form
+function runEnter() {
+
+    //prevent the form from reloading
+    d3.event.preventDefault();
+
+    // get the raw HTML node from the input element
+    var inputElement = d3.select("#datetime");
+
+    //get the value from that input
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    //use the filter to filter the data by date
+    var filteredData = tableData.filter(ufoSightings => ufoSightings.datetime === inputValue);
+    console.log(filteredData);
+};
